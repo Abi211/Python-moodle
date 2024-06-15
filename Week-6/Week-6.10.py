2f96e6dc41ce6ae387cad5667d7266fa63d986ee
Abi211 <231001004@rajalakshmi.edu.in>
2024-06-15 14:04:59 +0530

    Update Week-6.10.py

--- Week-6/Week-6.10.py
+++ Week-6/Week-6.10.py
@@ -1,7 +1,7 @@
 Username Domain Extension
 
 Given a string S which is of the format USERNAME@DOMAIN.EXTENSION, the program must print the EXTENSION, DOMAIN, USERNAME in the reverse order.
-
+ 
 
 
 Input Format:
